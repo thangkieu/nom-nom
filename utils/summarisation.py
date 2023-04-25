d37d5c4e499af4f913a7e9cb07025d0fd6d5f5ee
Thang Kieu <thang.kieu@2359media.com>
2023-04-25 14:39:38 +0800

    update configuration

--- utils/summarisation.py
+++ utils/summarisation.py
@@ -5,19 +5,26 @@ from utils.llm_models import summarise
 
 template = """
     You are an AI assistant that helps users summarise their text.
-    
-    Write a concise summary of the following text with a {tone} tone:
+
+    Write a concise summary of the following text with a {tone} tone, limit in {length} words:
     {text}
 """
 
-if 'summary' not in st.session_state:
-    st.session_state['summary'] = ''
+
+def init_session_data():
+    if 'summary' not in st.session_state:
+        st.session_state['summary'] = ''
+    if 'generated' not in st.session_state:
+        st.session_state['generated'] = False
 
 
 def write_summarisation():
+    init_session_data()
+
     text = ''
     tone = 'Neutral'
     paragraph_no = 3
+    length = 20
 
     with st.expander('Insert your text here', True):
         text = st.text_area('summarisation textarea',
@@ -25,18 +32,20 @@ def write_summarisation():
 
     with st.expander('Summarising\'s Configuration', False):
         tone = st.radio('Tone', ('Formal', 'Neutral', 'Humour'), index=1)
+        length = st.number_input(
+            'Length (no. of words)', step=50, min_value=20, format='%d')
 
     st.divider()
 
     is_generate = st.button(
         'Summarise Text', disabled=not text, type="primary")
 
-    prompt = PromptTemplate(input_variables=["text", "tone"],
+    prompt = PromptTemplate(input_variables=["text", "tone", "length"],
                             template=template)
-    prompt_str = prompt.format(text=text, tone=tone)
+    prompt_str = prompt.format(text=text, tone=tone, length=length)
 
     if is_generate:
-        st.session_state['summary'] = ''
+        st.session_state['generated'] = False
 
         with st.spinner('Summarising...'):
             azure_resp = summarise(prompt_str)
@@ -44,10 +53,15 @@ def write_summarisation():
 
             st.subheader('Summarised')
             st.success(azure_resp)
+
+            st.session_state['generated'] = True
+
     elif st.session_state['summary']:
         st.subheader('Summarised')
         st.success(st.session_state['summary'])
 
-    view_prompt = st.button('View Prompt')
-    if view_prompt:
-        st.info(template)
+    if st.session_state['generated']:
+        st.divider()
+        view_prompt = st.button('View Prompt')
+        if view_prompt:
+            st.info(template)
